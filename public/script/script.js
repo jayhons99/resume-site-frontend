1dea86d983393a4f2efeb079c1a48c7f9c5a8b66
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-03-29 18:08:38 -0400

    count tracker working

--- public/script/script.js
+++ public/script/script.js
@@ -1,10 +1,40 @@
 const URL = "https://tiijwog187.execute-api.us-east-1.amazonaws.com/Prod/";
 
 async function getData(url) {
-  const response = await fetch(url);
-
-  const data = response.json();
-  console.log(data);
+  fetch(url, { method: "GET" })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("NETWORK NOT RESPONDING");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      displayCount(data);
+    })
+    .catch((error) => console.error(`CANNOT FETCH DATA: ${error}`));
 }
 
-getData(URL);
+async function displayCount(data) {
+  const num = data.numOfVisits;
+  const countDiv = await document.getElementById("count");
+  countDiv.innerHTML = num;
+}
+
+async function updateCount(url) {
+  fetch(url, { method: "POST" })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("POST req failed");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      getData(url);
+    })
+    .catch((error) => console.error(`CANNOT UPDATE DATA: ${error}`));
+}
+window.onload = updateCount(URL);
